f457583212b1f5f8c31704ec3f05583d2aa0ae85
Dark-303 <156324532+Dark-303@users.noreply.github.com>
2024-11-21 16:28:45 -0500

    3 files made
    - logic
    - io
    - sim

---
+++ src/main/java/frc/robot/subsystems/elevatorWithArm/ElevatorWithArm.java
@@ -0,0 +1,7 @@
+package frc.robot.subsystems.elevatorWithArm;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ElevatorWithArm extends SubsystemBase{
+    
+}
---
+++ src/main/java/frc/robot/subsystems/elevatorWithArm/ElevatorWithArmIO.java
@@ -0,0 +1,5 @@
+package frc.robot.subsystems.elevatorWithArm;
+
+public interface ElevatorWithArmIO {
+    
+}
