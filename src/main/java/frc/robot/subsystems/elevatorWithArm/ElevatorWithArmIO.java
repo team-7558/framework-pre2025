bc735d8c021dd9bc6482423648baafaa207eca72
Dark-303 <156324532+Dark-303@users.noreply.github.com>
2024-11-23 14:24:30 -0500

    fixing adding and creating

--- src/main/java/frc/robot/subsystems/elevatorWithArm/ElevatorWithArmIO.java
+++ src/main/java/frc/robot/subsystems/elevatorWithArm/ElevatorWithArmIO.java
@@ -1,5 +1,20 @@
 package frc.robot.subsystems.elevatorWithArm;
 
+import org.littletonrobotics.junction.AutoLog;
+
 public interface ElevatorWithArmIO {
-    
+    @AutoLog
+    public static class ElevatorWithArmIOInputs {
+        public double elevPositionM = 0.0;
+        public double armPositionM = 0.0;
+        public double velocityMPerSecond = 0.0;
+        public double appliedVolts = 0.0;
+        public double[] currentAmps = new double[] {};
+    }
+
+    public default void updateInputs(ElevatorWithArmIOInputs inputs) {}
+    public default void setVoltageElev(double voltage_V) {}
+    public default void setVoltageArm(double voltage_V) {}
+    public default void setPosElev(double elevPositionM) {}
+    public default void setPosArm(double armPositionM) {}
 }
