89b07e8b72b01214c92d82e38846fb155b7590ec
Raymond Li <raymond.countrylane@gmail.com>
2024-12-15 08:43:36 -0500

    Latest

--- src/main/java/frc/robot/subsystems/pinkarm/pinkarm.java
+++ src/main/java/frc/robot/subsystems/pinkarm/pinkarm.java
@@ -23,155 +23,138 @@ import org.littletonrobotics.junction.Logger;
 
 public class Pinkarm extends StateMachineSubsystemBase<elevModes> {
 
-  private final Pinkarm2d mech = new Pinkarm2d("ArmActual", new Color8Bit(100, 0, 0));
-  private final Pinkarm2d targetmech = new Pinkarm2d("ArmTarget", new Color8Bit(0, 0, 100));
+    private static Pinkarm instance;
 
-  private static Pinkarm instance;
-  private final PinkarmIO io;
-  private AltTimer timer = new AltTimer();
+    private final PinkarmIO io;
+    private final Pinkarm2d mech = new Pinkarm2d("ArmActual", new Color8Bit(100, 0, 0));
+    private final Pinkarm2d targetMech = new Pinkarm2d("ArmTarget", new Color8Bit(0, 0, 100));
 
-  public static Pinkarm getInstance() {
-    if (instance == null) {
-      switch (Constants.currentMode) {
-        case REAL:
-          break;
-        case SIM:
-          instance = new Pinkarm(new PinkarmIOSim());
-          break;
-        default:
-          break;
-      }
+    private final PinkarmInputsAutoLogged inputs = new PinkarmInputsAutoLogged();
+    private final AltTimer timer = new AltTimer();
+
+    private double targetLengthMeters;
+    private double targetAngleDegrees;
+
+    public static final double ELEV_MIN_HEIGHT_M = 0.5;
+    public static final double ELEV_MAX_HEIGHT_M = 1.5;
+    public static final double ELEV_MIN_ANGLE_DEG = 0;
+    public static final double ELEV_MAX_ANGLE_DEG = 180;
+
+    private TrapezoidProfile.State elevSetpoint;
+    private TrapezoidProfile.State elevStartPoint;
+    private TrapezoidProfile.State elevGoal;
+
+    private TrapezoidProfile.State armSetpoint;
+    private TrapezoidProfile.State armStartPoint;
+    private TrapezoidProfile.State armGoal;
+
+    private final TrapezoidProfile.Constraints elevConstraints = new TrapezoidProfile.Constraints(1, 0.5);
+    private final TrapezoidProfile.Constraints armConstraints = new TrapezoidProfile.Constraints(90, 45);
+
+    private final TrapezoidProfile elevProfile = new TrapezoidProfile(elevConstraints);
+    private final TrapezoidProfile armProfile = new TrapezoidProfile(armConstraints);
+
+    private Pinkarm(PinkarmIO io) {
+        super("pinkarm");
+        this.io = io;
+        setTargetLength(0.7);
+        queueState(elevModes.IDLE);
     }
-    return instance;
-  }
 
-  private final PinkarmInputsAutoLogged inputs = new PinkarmInputsAutoLogged();
-  private double targetlength_m; // Added missing variable
-  private double targetangle_deg;
-  private double magnitude;
-  private double angle;
-
-  public static final double ELEV_MIN_HEIGHT_M = 0.5;
-  public static final double ELEV_MAX_HEIGHT_M = 1.5;
-  public static final double ELEV_MIN_ANGLE_DEG = 0;
-  public static final double ELEV_MAX_ANGLE_DEG = 180;
-
-  private TrapezoidProfile.State elevSetpoint;
-  private TrapezoidProfile.State elevStartpoint;
-  private TrapezoidProfile.State elevGoal;
-
-  private TrapezoidProfile.State armSetpoint;
-  private TrapezoidProfile.State armStartpoint;
-  private TrapezoidProfile.State armGoal;
-
-  private TrapezoidProfile.Constraints elevConstraints = new TrapezoidProfile.Constraints(1, 0.5);
-  private TrapezoidProfile.Constraints armConstraints = new TrapezoidProfile.Constraints(90, 45);
-
-  private TrapezoidProfile elevProfile = new TrapezoidProfile(elevConstraints);
-  private TrapezoidProfile armProfile = new TrapezoidProfile(armConstraints);
-
-  private Pinkarm(PinkarmIO io) {
-    super("pinkarm");
-    this.io = io;
-    setTargetLength(0.7);
-    queueState(elevModes.IDLE);
-  }
-
-  @Override
-  public void inputPeriodic() {
-    io.updateInputs(inputs);
-    Logger.processInputs("pinkarm", inputs);
-  }
-
-  @Override
-  public void handleStateMachine() {
-    switch (getState()) {
-      case DISABLED:
-        break;
-      case IDLE:
-        break;
-      case TRAVELLING:
-        if ((Math.abs(targetlength_m - inputs.elev_posMeters) < 0.5)
-            && (Math.abs(inputs.arm_posDegrees - targetangle_deg) < 5)) {
-          queueState(elevModes.HOLDING);
-        } else {
-
-          if (stateInit()) {
-            timer.reset();
-
-            armGoal = new TrapezoidProfile.State(targetangle_deg, 0);
-            elevGoal = new TrapezoidProfile.State(targetlength_m, 0);
-
-            elevStartpoint = new TrapezoidProfile.State(inputs.elev_posMeters, inputs.elev_velMPS);
-            armStartpoint = new TrapezoidProfile.State(inputs.arm_posDegrees, inputs.arm_velDegPS);
-          }
-
-          elevSetpoint = elevProfile.calculate(timer.time(), elevStartpoint, elevGoal);
-
-          armSetpoint = armProfile.calculate(timer.time(), armStartpoint, armGoal);
+    public static Pinkarm getInstance() {
+        if (instance == null) {
+            switch (Constants.currentMode) {
+                case SIM:
+                    instance = new Pinkarm(new PinkarmIOSim());
+                    break;
+                case REAL:
+                default:
+                    break;
+            }
         }
-
-        break;
-      case HOLDING:
-        io.setArmVoltage(0);
-        io.setelevVoltage(0);
-        break;
-      default:
-        break;
-    }
-  }
-
-  @Override
-  public void outputPeriodic() {
-
-    io.goToPos(elevSetpoint.position);
-    mech.setLength(inputs.elev_posMeters);
-    targetmech.setLength(elevSetpoint.position);
-
-    io.goToAngle(armSetpoint.position);
-    mech.setAngle(inputs.arm_posDegrees);
-    targetmech.setAngle(armSetpoint.position);
-
-    mech.periodic();
-    targetmech.periodic();
-
-    Logger.recordOutput("pinkarm/Targetlength_m", targetlength_m);
-    Logger.recordOutput("pinkarm/Targetangle_deg", targetangle_deg);
-    Logger.recordOutput("pinkarm/elevSetpoint", elevSetpoint.position);
-    Logger.recordOutput("pinkarm/armSetpoint", armSetpoint.position);
-  }
-
-  public void set(double meters, double angle) {
-
-    if (meters > ELEV_MAX_HEIGHT_M) {
-      meters = ELEV_MAX_HEIGHT_M;
-    } else if (meters < ELEV_MIN_HEIGHT_M) {
-      meters = ELEV_MIN_HEIGHT_M;
+        return instance;
     }
 
-    if (angle > ELEV_MAX_ANGLE_DEG) {
-      angle = ELEV_MAX_ANGLE_DEG;
-    } else if (angle < ELEV_MIN_ANGLE_DEG) {
-      angle = ELEV_MIN_HEIGHT_M;
+    @Override
+    public void inputPeriodic() {
+        io.updateInputs(inputs);
+        Logger.processInputs("pinkarm", inputs);
     }
 
-    setTargetLength(meters);
-    setTargetAngle(angle);
-  }
+    @Override
+    public void handleStateMachine() {
+        switch (getState()) {
+            case DISABLED:
+                break;
+            case IDLE:
+                break;
+            case TRAVELLING:
+                if (Math.abs(targetLengthMeters - inputs.elev_posMeters) < 0.5 &&
+                    Math.abs(inputs.arm_posDegrees - targetAngleDegrees) < 5) {
+                    queueState(elevModes.HOLDING);
+                } else {
+                    if (stateInit()) {
+                        timer.reset();
 
-  public void PlaceEndEffector(double x, double y) {
-    magnitude = Math.sqrt(0.1 * x * 0.1 * x + 0.1 * y * 0.1 * y);
-    angle = Units.radiansToDegrees(Math.atan2(0.1 * y, 0.1 * x));
+                        armGoal = new TrapezoidProfile.State(targetAngleDegrees, 0);
+                        elevGoal = new TrapezoidProfile.State(targetLengthMeters, 0);
 
-    set(magnitude, angle);
-  }
+                        elevStartPoint = new TrapezoidProfile.State(inputs.elev_posMeters, inputs.elev_velMPS);
+                        armStartPoint = new TrapezoidProfile.State(inputs.arm_posDegrees, inputs.arm_velDegPS);
+                    }
 
-  // Added a setter for targetHeight_m
-  public void setTargetLength(double targetlength_m) {
-    this.targetlength_m = targetlength_m;
-  }
+                    elevSetpoint = elevProfile.calculate(timer.time(), elevStartPoint, elevGoal);
+                    armSetpoint = armProfile.calculate(timer.time(), armStartPoint, armGoal);
+                }
+                break;
+            case HOLDING:
+                io.setArmVoltage(0);
+                io.setelevVoltage(0);
+                break;
+            default:
+                break;
+        }
+    }
 
-  public void setTargetAngle(double targetangle_deg) {
-    this.targetangle_deg = targetangle_deg;
-  }
+    @Override
+    public void outputPeriodic() {
+        io.goToPos(elevSetpoint.position);
+        mech.setLength(inputs.elev_posMeters);
+        targetMech.setLength(elevSetpoint.position);
+
+        io.goToAngle(armSetpoint.position);
+        mech.setAngle(inputs.arm_posDegrees);
+        targetMech.setAngle(armSetpoint.position);
+
+        mech.periodic();
+        targetMech.periodic();
+
+        Logger.recordOutput("pinkarm/TargetLengthMeters", targetLengthMeters);
+        Logger.recordOutput("pinkarm/TargetAngleDegrees", targetAngleDegrees);
+        Logger.recordOutput("pinkarm/ElevSetpoint", elevSetpoint.position);
+        Logger.recordOutput("pinkarm/ArmSetpoint", armSetpoint.position);
+    }
+
+    public void placeEndEffector(double x, double y) {
+        double magnitude = Math.sqrt(0.1 * x * 0.1 * x + 0.1 * y * 0.1 * y);
+        double angle = Units.radiansToDegrees(Math.atan2(0.1 * y, 0.1 * x));
+
+        set(magnitude, angle);
+    }
+
+    public void set(double meters, double angle) {
+        meters = Math.max(ELEV_MIN_HEIGHT_M, Math.min(ELEV_MAX_HEIGHT_M, meters));
+        angle = Math.max(ELEV_MIN_ANGLE_DEG, Math.min(ELEV_MAX_ANGLE_DEG, angle));
+
+        setTargetLength(meters);
+        setTargetAngle(angle);
+    }
+
+    public void setTargetLength(double targetLengthMeters) {
+        this.targetLengthMeters = targetLengthMeters;
+    }
+
+    public void setTargetAngle(double targetAngleDegrees) {
+        this.targetAngleDegrees = targetAngleDegrees;
+    }
 }
