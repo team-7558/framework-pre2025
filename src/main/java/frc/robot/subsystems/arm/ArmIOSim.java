c7391f5b190f49a9ff95a870fadd04763a91dc64
Raymond Li <raymond.countrylane@gmail.com>
2025-01-05 16:17:35 -0500

    Latest

--- src/main/java/frc/robot/subsystems/arm/ArmIOSim.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIOSim.java
@@ -3,5 +3,23 @@ package frc.robot.subsystems.arm;
 import frc.robot.subsystems.StateMachineSubsystemBase;
 
 public class ArmIOSim extends StateMachineSubsystemBase<ArmState> {
+
+    public ArmIOSim(String name) {
+        super(name);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void handleStateMachine() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handleStateMachine'");
+    }
+
+    @Override
+    protected void outputPeriodic() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'outputPeriodic'");
+    }
+
     
 }
