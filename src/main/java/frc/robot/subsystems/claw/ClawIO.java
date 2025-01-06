0c314343773554357eb17d85488345759a614f1a
Raymond Li <raymond.countrylane@gmail.com>
2025-01-06 17:40:58 -0500

    Latest

--- src/main/java/frc/robot/subsystems/claw/ClawIO.java
+++ src/main/java/frc/robot/subsystems/claw/ClawIO.java
@@ -6,6 +6,17 @@ import org.littletonrobotics.junction.AutoLog;
 public interface ClawIO {
   @AutoLog
   public static class ClawIOInputs {
-
+    public boolean open_or_not = true;
+    public double velocityMPS = 0.0;
+    public double volts_V = 0.0;
+    public double[] currents_A = new double[] {};
   }
+
+  public default updateInputs(ClawIOInputs inputs) {}
+
+  public default setVelocity(double velocity) {} 
+
+  public default setVoltage(double volts) {}
+
+  public default open(boolean open_or_not) {}
 }
