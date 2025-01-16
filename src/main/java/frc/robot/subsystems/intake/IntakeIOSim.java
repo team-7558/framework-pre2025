0b785e8fa3b09f4059d666a0d07abf1668ef4687
Dark-303 <156324532+Dark-303@users.noreply.github.com>
2025-01-16 16:22:31 -0500

    FIXED AT LAST

--- src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
@@ -7,11 +7,10 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 import frc.robot.util.AltTimer;
-import org.littletonrobotics.junction.Logger;
 
 public class IntakeIOSim implements IntakeIO {
 
-  private TrapezoidProfile.State armSetpoint;
+  public TrapezoidProfile.State armSetpoint;
   private TrapezoidProfile.State armStartPoint;
   private TrapezoidProfile.State armGoal;
 
@@ -28,7 +27,7 @@ public class IntakeIOSim implements IntakeIO {
   private final SingleJointedArmSim armSim =
       new SingleJointedArmSim(
           DCMotor.getKrakenX60Foc(1),
-          45,
+          1,
           3.67,
           0.5,
           Units.degreesToRadians(0),
@@ -38,11 +37,14 @@ public class IntakeIOSim implements IntakeIO {
   private final PIDController armPositionPID = new PIDController(70, 1, 4);
   private double arm_applied_volts = 0.0;
 
+  public IntakeIOSim() {}
+
   @Override
   public void updateInputs(IntakeIOInputs inputs) {
     // Update elevator simulation
     armSim.update(0.02); // 20 ms update
     inputs.slap_pos_deg = Units.radiansToDegrees(armSim.getAngleRads());
+    //System.out.println("print1 " + armSim.getAngleRads());
     inputs.slap_velDegPS = Units.radiansToDegrees(armSim.getVelocityRadPerSec());
     inputs.slap_volts = arm_applied_volts;
     inputs.slap_currents =
@@ -73,21 +75,20 @@ public class IntakeIOSim implements IntakeIO {
 
   @Override
   public void goToAngle(double degrees, IntakeIOInputs inputs, boolean first_time) {
+    //System.out.println("Is going to angle?");
     if (first_time) {
       // System.out.println("Travelling once");
       timer.reset();
-      armGoal = new TrapezoidProfile.State(degrees, 0);
-      armStartPoint = new TrapezoidProfile.State(inputs.slap_pos_deg, inputs.slap_velDegPS);
     }
-
+    armGoal = new TrapezoidProfile.State(degrees, 0);
+    armStartPoint = new TrapezoidProfile.State(inputs.slap_pos_deg, inputs.slap_velDegPS);
     armSetpoint = armProfile.calculate(timer.time(), armStartPoint, armGoal);
     // Check if the target is valid (optional safety check)
     double targetRadians = Units.degreesToRadians(armSetpoint.position);
     armPositionPID.setSetpoint(targetRadians);
     double calculatedVoltage = armPositionPID.calculate(armSim.getAngleRads());
     setArmVoltage(calculatedVoltage);
-
-    Logger.recordOutput("Intake/ArmSetpoint", armSetpoint.position);
+    //System.out.println("print2 " + armSim.getAngleRads());
   }
 
   @Override
